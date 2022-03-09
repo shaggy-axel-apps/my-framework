ba5e37ef0dd0f6de9ff8ed13ea19a4b0fa57e495
Ruslan <kornerus@fjfi.cvut.cz>
2022-03-09 04:59:03 +0300

    change project settings and urls

--- src/blog_project/settings.py
+++ src/blog_project/settings.py
@@ -4,11 +4,3 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
-ROOT_URLCONF = "blog_project.urls"
-
-INSTALLED_APPS = [
-    'apps.users',
-    'apps.posts',
-    'apps.stories',
-    'apps.videos',
-]
